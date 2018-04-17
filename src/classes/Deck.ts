da02ea949954a48a5f7b0ca88b33dd9018c5228e
Josh R. Dunlavy <jdunlavy@gmail.com>
2018-04-16 22:36:22 -0700

    style: improve code

--- src/classes/Deck.ts
+++ src/classes/Deck.ts
@@ -2,10 +2,10 @@ import ICard from "./../interfaces/ICard";
 
 export default class Deck {
 	private _cards:ICard[];
-	private _cardRanks:string[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-	private _cardSuits:string[] = ["♡", "♤", "♧", "♢"];
+	private readonly _cardRanks:string[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+	private readonly _cardSuits:string[] = ["♡", "♤", "♧", "♢"];
 
-	private _buildCards() {
+	private _buildCards():void {
 		this._cardRanks.forEach(rank => {
 			this._cardSuits.forEach(suit => {
 				this._cards.push({
@@ -23,7 +23,7 @@ export default class Deck {
 		this._buildCards();
 	}
 
-	public drawCard() {
+	public drawCard():ICard {
 		const randomCardIndex = Math.floor(Math.random() * this._cards.length);   // fancy psuedo-random stuff
 		const card = this._cards[randomCardIndex];                                // fetch myself a reference to the card I'm drawing
 		this._cards.splice(randomCardIndex, 1);                                   // remove it from the array so it won't be drawn again
@@ -31,7 +31,7 @@ export default class Deck {
 		return card;
 	}
 
-	public returnCardToDeck(card:ICard) {
+	public returnCardToDeck(card:ICard):void {
 		this._cards.push(card);
 	}
 }
