bc66228f3db659d9101d819c46bf233e0cc89466
benreckas <benreckas@gmail.com>
2018-04-18 09:19:07 -0700

    Init commit. Part 3 completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import Deck from "../classes/Deck";
+import ICard from "../interfaces/ICard";
+
 
 @Component({
 	selector: "application",
@@ -8,7 +11,23 @@ export class AppComponent implements OnInit {
 	// write your component code here; create the properties and methods you need to get the job
 	// done as described in "app.html"; start by importing modules you need such as "./../classes/Deck"
 
-	public ngOnInit():void {
+	public deck: Deck;
+	public hand: any[] = [];
+	public lastDrawnCard: ICard;
+	public card: ICard;
 
+	public ngOnInit():void {
+		this.deck = new Deck();
 	}
+
+	public getCard() {
+		this.lastDrawnCard = this.deck.drawCard()
+		this.hand.push(this.lastDrawnCard);
+	}
+
+	public returnCard(card) {
+		this.hand = this.hand.filter(elmnt => elmnt !== card);
+		this.deck.returnCardToDeck(card);
+	}
+
 }
